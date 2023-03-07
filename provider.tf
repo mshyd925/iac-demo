f2c5e823d657cba553c88798745070bf0f21e100
awsrepo423 <126638994+awsrepo423@users.noreply.github.com>
2023-03-07 09:42:21 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIA5SSWUWGMU5BCYBX6"
-  secret_key = "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"
+  access_key = "AKIA3GQN6PPRF23U4BPP"
+  secret_key = "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"
 }
