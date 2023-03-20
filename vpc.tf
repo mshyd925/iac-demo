68defd7a30335679803c19f9e9692368d078e0f6
mshyd925 <102269158+mshyd925@users.noreply.github.com>
2023-03-20 11:48:36 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,9 @@
 resource "aws_vpc" "main" {
-  cidr_block       = "50.0.0.0/16"
+  cidr_block       = "192.168.0.0/16"
   instance_tenancy = "default"
 
   tags = {
-    Name = "Hyd-Cloud-VPC"
+    Name = "Custom-VPC-C"
     Purpose = "Jenkins Demo"
   }
 }
