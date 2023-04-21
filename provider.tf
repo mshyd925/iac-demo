cf9ba86f5bf9966e5f368d4c63da202e6d9c4827
mshyd925 <102269158+mshyd925@users.noreply.github.com>
2023-04-21 07:46:55 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAQAUQZ3DMYPC63XJ6"
-  secret_key = "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"
+  access_key = "AKIAUQWDMX37DYK4WNJR"
+  secret_key = "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"
 }
